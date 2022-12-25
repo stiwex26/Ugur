d22cbd81c1495ca01666de7518224519c737a067
stiwex26 <wizard.botik@gmail.com>
2022-12-25 14:56:10 +0300

    /

---
+++ untilited2.py
@@ -0,0 +1,8 @@
+name=input("Please,enter your name")
+surname=input("Enter your surname")
+age=input("How old are you?")
+print("hello,Timur,Ugur")
+print ("Happy B-day")
+age=14
+age=age+1
+print("You are",age,"years old")
